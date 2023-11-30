4ef41bfa4f5bc9eaa28f12341c1f52d22bba9b88
Cookees81 <salazar-teresa@sbcglobal.net>
2023-11-30 01:05:44 -0800

    submission with errors

---
+++ Starter_Files/Resources/crowdfunding_db_schema.sql
@@ -0,0 +1,45 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "campaign" (
+    "cf_id" varchar(10)   NOT NULL,
+    "contact_id" varchar(10)   NOT NULL,
+    "company_name" varchar(200)   NOT NULL,
+    "description" varchar(200)   NOT NULL,
+    "goal" int   NOT NULL,
+    "pledged" int   NOT NULL,
+    "outcome" varchar(20)   NOT NULL,
+    "backers_count" int   NOT NULL,
+    "country" varchar(10)   NOT NULL,
+    "currency" varchar(10)   NOT NULL,
+    "launch_date" date   NOT NULL,
+    "end_date" date   NOT NULL,
+    "category_id" varchar(50)   NOT NULL,
+    "subcategory_id" varchar(50)   NOT NULL,
+    CONSTRAINT "pk_campaign" PRIMARY KEY (
+        "cf_id"
+     )
+);
+
+CREATE TABLE "category" (
+    "category_id" varchar(50)   NOT NULL,
+    "category" varchar(50)   NOT NULL
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" varchar(50)   NOT NULL,
+    "subcategory" varchar(50)   NOT NULL
+);
+
+CREATE TABLE "contact" (
+    "serial" id   NOT NULL,
+    "text" varchar(200)   NOT NULL
+);
+
+ALTER TABLE "category" ADD CONSTRAINT "fk_category_category_id" FOREIGN KEY("category_id")
+REFERENCES "campaign" ("category_id");
+
+ALTER TABLE "subcategory" ADD CONSTRAINT "fk_subcategory_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "campaign" ("subcategory_id");
+
